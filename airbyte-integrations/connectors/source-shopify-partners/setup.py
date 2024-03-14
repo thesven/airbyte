7cd76f68a655efcb76b49030ea5a54e63a95652f
michael svendsen <michael@mikesvendsen.net>
2024-03-14 09:42:01 -0400

    working on publish

--- airbyte-integrations/connectors/source-shopify-partners/setup.py
+++ airbyte-integrations/connectors/source-shopify-partners/setup.py
@@ -1,30 +1,30 @@
-#
-# Copyright (c) 2023 Airbyte, Inc., all rights reserved.
-#
-
-
 from setuptools import find_packages, setup
 
 MAIN_REQUIREMENTS = [
-    "airbyte-cdk~=0.11",
+    "airbyte-cdk~=0.2",
+    "paramiko==2.11.0",
+    "backoff==1.8.0",
+    "terminaltables==3.1.0",
+    "pandas==1.5.0",
 ]
 
 TEST_REQUIREMENTS = [
     "requests-mock~=1.9.3",
-    "pytest~=6.1",
     "pytest-mock~=3.6.1",
+    "pytest~=6.1",
+    "docker==5.0.3",
 ]
 
 setup(
     entry_points={
         "console_scripts": [
-            "source-zoom=source_zoom.run:run",
+            "source-shopify-partners=source_shopify_partners_old.run:run",
         ],
     },
-    name="source_zoom",
-    description="Source implementation for Zoom.",
-    author="Airbyte",
-    author_email="contact@airbyte.io",
+    name="source_shopify_partners_old",
+    description="Source implementation for Shopify Partners API",
+    author="ReactivApp",
+    author_email="mike@reactivapp.com",
     packages=find_packages(),
     install_requires=MAIN_REQUIREMENTS,
     package_data={
